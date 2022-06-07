1153a5942f317cec6f2137ab6414785e3c7402d3
ArnautVasseur <vasseur.arnaut@gmail.com>
2022-06-08 00:04:50 +0200

    Corriger les fautes d'eden

--- src/App.vue
+++ src/App.vue
@@ -8,7 +8,7 @@ import { RouterLink, RouterView } from 'vue-router'
     <p class="text-white text-2xl font-bold leading-7 mb-5 tracking-tight">Un objet ne vous plait plus
       et vous souhaitez vous en 
       débarasser?</p>
-    <p class="text-white tracking-tighter mb-5">Renovae s’engage à donner une nouvelle vie à vos objets en vous mettant en relation avec des artistes au quatres coins de la france !</p>
+    <p class="text-white tracking-tighter mb-5">Renova s’engage à donner une nouvelle vie à vos objets en vous mettant en relation avec des artistes au quatres coins de la france !</p>
     <a href="http://renova.arnaut.fr">
       <button class="border-2 border-orange-300 shadow-lg p-2 rounded-xl bg-orange-300 text-white mr-5">
         S'inscrire
@@ -20,10 +20,10 @@ import { RouterLink, RouterView } from 'vue-router'
   </div>
   <div class="bg-orange-300 w-[375px] h-[500px] p-5">
     <p class="text-white font-bold text-2xl mb-5 leading-7">2 Millions de tonnes de mobilier
-sont jeter en France par an</p>
+      sont jetés en France par an</p>
 
-    <p class="text-white font-semibold leading-5 mb-2">Au lieu de jeter vos anciens meubles pourquoi ne pas les confier à Renovae ?</p>
-    <p class="text-white leading-5 font-light mb-5">Renovae vous permetterait de re-donner une nouvelle vie à vos objets en les rendants accessible à tous !</p>
+    <p class="text-white font-semibold leading-5 mb-2">Au lieu de jeter vos anciens meubles pourquoi ne pas les confier à Renova ?</p>
+    <p class="text-white leading-5 font-light mb-5">Renova vous permetterait de re-donner une nouvelle vie à vos objets en les rendants accessible à tous !</p>
     <a href="http://renova.arnaut.fr">
     <button class="bg-white p-2 rounded-xl mb-10">
       Rejoindre l'aventure
@@ -35,7 +35,7 @@ sont jeter en France par an</p>
      <p class="text-white text-2xl font-bold leading-7 mb-5 tracking-tight mt-24">Un objet ne vous plait plus
       et vous souhaitez vous en 
       débarasser?</p>
-    <p class="text-white tracking-tighter mb-5">Renovae s’engage à donner une nouvelle vie à vos objets en vous mettant en relation avec des artistes au quatres coins de la france !</p>
+    <p class="text-white tracking-tighter mb-5">Renova s’engage à donner une nouvelle vie à vos objets en vous mettant en relation avec des artistes au quatres coins de la france !</p>
   </div>
   
   
