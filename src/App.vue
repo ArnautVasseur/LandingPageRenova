bf9d289f45ad5034106c81e9f2cfb55006fc0334
ArnautVasseur <vasseur.arnaut@gmail.com>
2022-06-07 23:21:45 +0200

    Upload FIles

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+</script>
+
+<template>
+  <img src="../public/jonathan-borba-mHTFGuCXREQ-unsplash1.webp" alt="image1" class="w-[375px] absolute">
+  <p class="absolute">ygszbfbzu</p>
+</template>
+
+<style>
+  body{
+    width: 100%;
+    margin: 0;
+  }
+  html{
+    margin: 0;
+    height: 100%;
+    width: 100%;
+  }
+</style>
